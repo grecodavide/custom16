3d5d1a3b99fe97f5f9681c22fc8b68f76961c01c
Davide Greco <grecodavide.work@proton.me>
2024-12-28 10:27:28 +0100

    feat: specify palette path in opts

--- colors/custom16.lua
+++ colors/custom16.lua
@@ -1,6 +1,3 @@
-vim.o.background = "light"
-
-vim.cmd("silent !kill -SIGUSR1 $(pgrep kitty) &")
 package.loaded["custom16"] = nil
-package.loaded["custom16.palette"] = nil
+package.loaded[_G.custom16_palette_path] = nil
 require("custom16").set_colors()
